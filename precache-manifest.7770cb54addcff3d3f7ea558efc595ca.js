d4a1043457c60e580f0c89d322dda75cd1fdcaf0
mishelpa <judith.paredesartica@gmail.com>
2020-07-07 07:28:10 -0500

    Updates

---
+++ precache-manifest.7770cb54addcff3d3f7ea558efc595ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be705d0c42997a8ed68a73a7d5413515",
+    "url": "/chat/index.html"
+  },
+  {
+    "revision": "b5dc97b236f75b9ed886",
+    "url": "/chat/static/css/main.a68cb778.chunk.css"
+  },
+  {
+    "revision": "1b43a91178c88e81b979",
+    "url": "/chat/static/js/2.5529a02b.chunk.js"
+  },
+  {
+    "revision": "abf03617803901bc022560d39b5885ef",
+    "url": "/chat/static/js/2.5529a02b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5dc97b236f75b9ed886",
+    "url": "/chat/static/js/main.8aa3de75.chunk.js"
+  },
+  {
+    "revision": "aae4c2bf76eeeea45bf5",
+    "url": "/chat/static/js/runtime-main.0b174478.js"
+  }
+]);
